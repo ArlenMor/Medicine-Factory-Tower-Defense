0aa411020af9ed9e2d03d406cf2c21d94d5796f5
Arlen <Demonik4053@gmail.com>
2026-05-19 00:11:40 +0700

    [Shop] - warning when not enough coin for buy

--- Assets/_Project/Code/Scripts/FieldSystem/DefenseBuyButtonView.cs
+++ Assets/_Project/Code/Scripts/FieldSystem/DefenseBuyButtonView.cs
@@ -1,3 +1,4 @@
+using _Project.Code.Scripts.Data;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +7,9 @@ namespace _Project.Code.Scripts.BattleField
 {
     public class DefenseBuyButtonView : MonoBehaviour
     {
+        private static readonly Color ColorAffordable    = Color.white;
+        private static readonly Color ColorNotAffordable = new Color(0x9A / 255f, 0x23 / 255f, 0x24 / 255f);
+
         [SerializeField] private DefenseType _defenseType;
         [SerializeField] private Image _icon;
         [SerializeField] private TMP_Text _priceText;
@@ -18,6 +22,22 @@ namespace _Project.Code.Scripts.BattleField
             Price = data.CreditCost;
             if (_icon != null) _icon.sprite = data.Icon;
             if (_priceText != null) _priceText.text = data.CreditCost.ToString();
+
+            GameData.Instance.OnResourcesChanged += RefreshPriceColor;
+            RefreshPriceColor();
+        }
+
+        private void OnDestroy()
+        {
+            if (GameData.Instance != null)
+                GameData.Instance.OnResourcesChanged -= RefreshPriceColor;
+        }
+
+        private void RefreshPriceColor()
+        {
+            if (_priceText == null) return;
+            int credits = GameData.Instance.Resources[ResourceType.Credit];
+            _priceText.color = credits >= Price ? ColorAffordable : ColorNotAffordable;
         }
     }
 }
